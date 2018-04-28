f5bf1e5c622d6c699ee8ceadc28a0b472a8fe468
Patatoa I <patatoa@gmail.com>
2018-04-28 18:57:03 -0500

    added test for incorrect http method

--- src/main_test.go
+++ src/main_test.go
@@ -44,3 +44,30 @@ func TestHandler(t *testing.T) {
 		t.Errorf("Response should be %s, got %s", expected, respString)
 	}
 }
+
+func TestRouterForNonExistentRoute(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+	resp, err := http.Post(mockServer.URL+"/hello", "", nil)
+
+	// In case there is an error in forming the request, we fail and stop the test
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("Status should be 405, got %d", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	respString := string(b)
+	expected := ""
+
+	if respString != expected {
+		t.Errorf("Response should be %s, got %s", expected, respString)
+	}
+}
